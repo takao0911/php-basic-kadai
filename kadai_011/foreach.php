1c24e5ceea627a6c9642ae4c06945a8dfc22687e
takao0911 <th.mfd1012@gmail.com>
2025-02-13 16:35:02 +0900

    提出

---
+++ kadai_011/foreach.php
@@ -0,0 +1,13 @@
+<?php
+
+$vegetable = array(
+    '名前' => '玉ねぎ',
+    '値段' => 200,
+    '産地' => '北海道'
+);
+
+
+foreach ($vegetable as $key => $value) {
+    echo $key . ":" . $value . "<br>"; 
+}
+?>
